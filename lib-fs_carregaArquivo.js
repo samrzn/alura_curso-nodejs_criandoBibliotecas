50285eb75c84e6735b1a598ad0edb0c493843fb7
samrzn <sam_profissional@outlook.com>
2023-06-20 15:48:17 -0300

    refactor style-formatacao lib Chalk

--- lib-fs_carregaArquivo.js
+++ lib-fs_carregaArquivo.js
@@ -5,7 +5,7 @@ function uploadArquivo(pathArquivo) {
     const encoding = 'utf-8';
     fs.readFile(pathArquivo, encoding, (_, text) => {
         // primeiro parâmetro sendo ignorado _ (por hora) é o "error" (tratamento de erro).
-        console.log(chalk.green(text))
+        console.log(chalk.greenBright.bgWhite(text))
     })
 }
 
