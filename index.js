a8d4f107e3974ac22ad657432bc6a5b2d3720342
samrzn <sam_profissional@outlook.com>
2023-06-20 16:13:41 -0300

    refactor identacao

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 console.log(chalk.blue.bgWhite.bold('olá mundo!'));
 console.log(chalk.yellowBright('A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.'));
 console.log(chalk.blue('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'));
- */
+******************************************************************************************************************************************************************************************** 
+*/
 
 
 // Aula - A lib fs
@@ -22,6 +23,7 @@ function uploadArquivo(pathArquivo) {
 }
 
 uploadArquivo('./arquivos/texto.md');
+********************************************************************************************************************************************************************************************
  */
 
 
@@ -38,7 +40,7 @@ function treatErro(error) {
 function uploadArquivo(pathArquivo) {
     const encoding = 'utf-8';
     fs.readFile(pathArquivo, encoding, (error, text) => {
-        if(error) {
+        if (error) {
             treatErro(error);
         }
         console.log(chalk.green.bgWhiteBright(text));
