455bfdf432af84f82755493dd2e5ab41a6956134
samrzn <sam_profissional@outlook.com>
2023-06-21 17:39:12 -0300

    feat extractLink expressoes regulares

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function uploadArquivo(pathArquivo) {
 }
 
 uploadArquivo('./arquivos/');
+********************************************************************************************************************************************************************************************
  */
 
 
@@ -72,6 +73,7 @@ function uploadArquivo(pathArquivo) {
 }
 
 uploadArquivo('./arquivos/texto.md');
+********************************************************************************************************************************************************************************************
  */
 
 
@@ -97,12 +99,13 @@ async function uploadArquivo(pathArquivo) {
 
 uploadArquivo('./arquivos/texto.md');
 uploadArquivo('./arquivos/');
+********************************************************************************************************************************************************************************************
  */
 
 
 // Aula - try, catch e finally
 
-import fs from 'fs';
+/* import fs from 'fs';
 import chalk from 'chalk';
 
 function treatErro(error) {
@@ -119,11 +122,80 @@ async function uploadArquivo(pathArquivo) {
         treatErro(error);
     } finally {
         console.log(chalk.yellow('operação concluída'));
-        /*  Lembrando que todo o código dentro do bloco finally sempre será executado. 
-            Não fará diferença se o processamento tiver sido efetuado com sucesso (o código manteve-se
-            dentro do bloco try) ou tiver gerado alguma exceção (o código foi lançada ao bloco catch).   */
+        //  Lembrando que todo o código dentro do bloco finally sempre será executado. 
+        //  Não fará diferença se o processamento tiver sido efetuado com sucesso (o código manteve-se
+        //  dentro do bloco try) ou tiver gerado alguma exceção (o código foi lançada ao bloco catch).
     }
 }
 
 uploadArquivo('./arquivos/texto.md');
 uploadArquivo('./arquivos/');
+********************************************************************************************************************************************************************************************
+ */
+
+
+// Aula - Capturando grupos
+
+/* const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.';
+
+function extractLink(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captura = text.match(regex);
+    console.log(captura);
+}
+
+extractLink(textoTeste);
+ */
+
+
+// Aula - Retornando links
+
+/* const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.';
+
+function extractLinkArraysObject(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captura = [...text.matchAll(regex)];
+    console.log(captura);
+    // Essa função retorna um array repleto de arrays objetos iteráveis
+}
+
+extractLinkArraysObject(textoTeste);
+
+
+function extractLink(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captura = [...text.matchAll(regex)];
+    const resultado = captura.map(print => ({ [print[1]]: print[2] }))
+    console.log(resultado);
+    // Essa funçao retorna um array de objetos com índices específicos dos arrays iteráveis
+}
+
+extractLink(textoTeste); */
+
+
+import fs from 'fs';
+import chalk from 'chalk';
+
+function treatErro(error) {
+    console.log(error);
+    throw new Error(chalk.red(error.code, 'Erro de diretório: arquivo não localizado.'));
+}
+
+function extractLink(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captura = [...text.matchAll(regex)];
+    const resultado = captura.map(print => ({ [print[1]]: print[2] }));
+    return resultado;
+}
+
+async function uploadArquivo(pathArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const text = await fs.promises.readFile(pathArquivo, encoding)
+        console.log(extractLink(chalk.green(text)));
+    } catch (error) {
+        treatErro(error);
+    }
+}
+
+uploadArquivo('./arquivos/texto.md');
